3b0e840479383174c2716f83197b871939095a9b
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-11-19 10:50:44 +0530

    Added testimonials section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/frontend/ui/HeroSection'
+import Testimonial from '@/components/frontend/ui/Testimonial'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <HeroSection/>
+      <Testimonial/>
     </div>
   )
 }
